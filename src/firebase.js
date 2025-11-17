5d917ea82eff3198da825bab01a80f34e5ef296e
77Svan <spencerwang4014@gmail.com>
2025-11-17 16:23:04 -0600

    Firebase and Git and React

---
+++ src/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Analytics only in browser environment (commented out for now)
+// let analytics;
+// if (typeof window !== 'undefined') {
+//   analytics = getAnalytics(app);
+// }
+
+// Export app so it can be used in other files (for Firestore, Auth, etc.)
+export { app };
+// export { app, analytics }; // Uncomment if you enable analytics

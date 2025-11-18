7d54a52c1faf5fa69cc489524aaaa06993d5dd87
77Svan <spencerwang4014@gmail.com>
2025-11-17 21:03:15 -0600

    Render Page Render

---
+++ src/unAuth/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1 className="landing-title">Hello Landing Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
+

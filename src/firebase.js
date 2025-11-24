1d9cf81fd6cc88f36ce8cb80b3102f1d200cbe1a
77Svan <spencerwang4014@gmail.com>
2025-11-23 20:09:21 -0600

    Python and Emulators

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,10 +19,18 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const functions = getFunctions(app);
+if (
+  window.location.hostname === 'localhost' ||
+  process.env_REACT_APP_ENV === 'local'
+) {
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+}
+export {app, analytics, functions}
 
 
-let analytics;
 if (typeof window !== 'undefined') {
   analytics = getAnalytics(app);
 }
-export {app, analytics}
